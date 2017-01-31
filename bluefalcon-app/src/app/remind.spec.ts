90e8dfabfbd9a133984b6bebaf55257ace3ab709
flex <flexedoyster@hotmail.com>
2017-01-31 17:55:35 -0500

    Created Remind class with constructor that creates a remind with an id, title, time, and a repeat option. Also included unit tests to test the constructor and object.

--- bluefalcon-app/src/app/remind.spec.ts
+++ bluefalcon-app/src/app/remind.spec.ts
@@ -4,4 +4,20 @@ describe('Remind', () => {
   it('should create an instance', () => {
     expect(new Remind()).toBeTruthy();
   });
+
+  it('should accept values in the constructor', () => {
+    var date = new Date('December 25, 1987');
+    let remind = new Remind({
+      title: 'hello',
+      complete: true,
+      time: date,
+      repeat:15
+    });
+    console.log(remind);
+    console.log(remind.date);
+    expect(remind.title).toEqual('hello');
+    expect(remind.complete).toEqual(true);
+    expect(remind.time).toEqual(date);
+    expect(remind.repeat).toEqual(15);
+  });
 });
--- bluefalcon-app/src/app/remind.ts
+++ bluefalcon-app/src/app/remind.ts
@@ -1,2 +1,12 @@
 export class Remind {
+  id: number;
+  title: string = '';
+  time: Date;
+  repeat: number;
+
+  complete: boolean = false;
+
+  constructor(values: Object = {}) {
+    Object.assign(this, values);
+  }
 }
