17d4997c382c1a309a126ab5484e27c417a04cbc
MWessel27 <mwessel27@gmail.com>
2017-02-01 21:41:03 -0500

    Updated main view component to show the app. Bootstrap is not working properly, but all functionality is there. Unit tests for view are included.

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -2,10 +2,15 @@
 
 import { TestBed, async } from '@angular/core/testing';
 import { AppComponent } from './app.component';
+import { FormsModule } from '@angular/forms';
+import { Remind } from './remind';
 
 describe('AppComponent', () => {
   beforeEach(() => {
     TestBed.configureTestingModule({
+      imports: [
+        FormsModule
+      ],
       declarations: [
         AppComponent
       ],
@@ -19,16 +24,16 @@ describe('AppComponent', () => {
     expect(app).toBeTruthy();
   }));
 
-  it(`should have as title 'app works!'`, async(() => {
-    const fixture = TestBed.createComponent(AppComponent);
-    const app = fixture.debugElement.componentInstance;
-    expect(app.title).toEqual('app works!');
+  it(`should have a newRemind remind`, async(() => {
+    let fixture = TestBed.createComponent(AppComponent);
+    let app = fixture.debugElement.componentInstance;
+    expect(app.newRemind instanceof Remind).toBeTruthy()
   }));
 
-  it('should render title in a h1 tag', async(() => {
-    const fixture = TestBed.createComponent(AppComponent);
+  it('should display "Reminders" in h1 tag', async(() => {
+    let fixture = TestBed.createComponent(AppComponent);
     fixture.detectChanges();
-    const compiled = fixture.debugElement.nativeElement;
-    expect(compiled.querySelector('h1').textContent).toContain('app works!');
+    let compiled = fixture.debugElement.nativeElement;
+    expect(compiled.querySelector('h1').textContent).toContain('Reminders');
   }));
 });
--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,39 @@
-import { Component } from '@angular/core';
+import {Component} from '@angular/core';
+import {Remind} from './remind';
+// Import class so we can register it as dependency injection token
+import {RemindDataService} from './remind-data.service';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrls: ['./app.component.css'],
+  providers: [RemindDataService]
 })
 export class AppComponent {
-  title = 'app works!';
+
+  newRemind: Remind = new Remind();
+
+  // Ask Angular DI system to inject the dependency
+  // associated with the dependency injection token `RemindDataService`
+  // and assign it to a property called `remindDataService`
+  constructor(private remindDataService: RemindDataService) {
+  }
+
+  addRemind() {
+    this.remindDataService.addRemind(this.newRemind);
+    this.newRemind = new Remind();
+  }
+
+  toggleRemindComplete(remind) {
+    this.remindDataService.toggleRemindComplete(remind);
+  }
+
+  removeRemind(remind) {
+    this.remindDataService.deleteRemindById(remind.id);
+  }
+
+  get reminds() {
+    return this.remindDataService.getAllReminds();
+  }
+
 }
