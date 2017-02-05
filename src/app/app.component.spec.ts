4148c64946ae413049c2f4670acec62fd683fbf7
MWessel27 <mwessel27@gmail.com>
2017-02-05 13:10:19 -0500

    Added more unit tests to the app component interface. Code coverage is now 100% for app component.

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -30,6 +30,36 @@ describe('AppComponent', () => {
     expect(app.newRemind instanceof Remind).toBeTruthy();
   }));
 
+  it(`should create a new reminder and update lastId`, async(() => {
+    const fixture = TestBed.createComponent(AppComponent);
+    const app = fixture.debugElement.componentInstance;
+    app.addRemind();
+    expect(app.remindDataService.lastId).toEqual(1);
+  }));
+
+  it(`should create a new reminder and remove the reminder`, async(() => {
+    const fixture = TestBed.createComponent(AppComponent);
+    const app = fixture.debugElement.componentInstance;
+    app.addRemind();
+    expect(app.remindDataService.lastId).toEqual(1);
+    let remind1 = app.remindDataService.getRemindById(1);
+    expect(app.remindDataService.getAllReminds()).toEqual([remind1])
+    app.removeRemind(remind1);
+    expect(app.remindDataService.getAllReminds()).toEqual([]);
+  }));
+
+  it(`should toggle the complete status of a remind`, async(() => {
+    const fixture = TestBed.createComponent(AppComponent);
+    const app = fixture.debugElement.componentInstance;
+    app.addRemind();
+    expect(app.remindDataService.lastId).toEqual(1);
+    const remind1 = app.remindDataService.getRemindById(1);
+    app.toggleRemindComplete(remind1);
+    expect(remind1.complete).toEqual(true);
+    app.toggleRemindComplete(remind1);
+    expect(remind1.complete).toEqual(false);
+  }));
+
   it('should display "Reminders" in h1 tag', async(() => {
     const fixture = TestBed.createComponent(AppComponent);
     fixture.detectChanges();
